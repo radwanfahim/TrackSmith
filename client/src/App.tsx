17a78cec39d6d94d8671057eeaa4cd2d70bcc0a5
Radwan Fahim <88280947+radwanfahim@users.noreply.github.com>
2025-12-11 13:13:29 +0600

    not found page added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Route, Router } from "@solidjs/router";
 import Home from "./components/Home/Home";
+import Dashboard from "./components/Dashboard/Dashboard";
+import NotFound from "./shared/NotFound/NotFound";
 
 function App() {
   return (
     <>
       <Router>
         <Route path="/" component={Home} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="*" component={NotFound} />
       </Router>
     </>
   );
